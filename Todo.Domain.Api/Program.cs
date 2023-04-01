114db6d7ac49d1143c6bb6a77b02a3e5f9047516
Adriano Mota <adrianowsh@hotmail.com>
2023-04-01 19:30:15 -0300

    add handler and repository dependencies

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -1,10 +1,20 @@
 using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+//builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+
+//Handlers
+builder.Services.AddTransient<TodoHandler, TodoHandler>();
+
+//Repository
+builder.Services.AddTransient<ITodoRepository, TodoRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
